066b5569d6de79fce4d94b557007e46a1e8e26d4
Lassi Haarala <laelhaar@jyu2601-31.ws.jyu.fi>
2026-07-29 12:08:14 +0300

    Added alternative sendMessageDelegateOne, used in ResearchVideo

---
+++ src/main/java/fi/jyu/vasara/tools/SendMessageDelegateOne.java
@@ -0,0 +1,8 @@
+package fi.jyu.vasara.tools;
+
+/**
+ * Compatibility alias for BPMN models that reference SendMessageDelegateOne.
+ *
+ * <p>Behavior is inherited from {@link SendMessageDelegate}.
+ */
+public class SendMessageDelegateOne extends SendMessageDelegate {}
